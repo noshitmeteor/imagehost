5913ab8ce05158fa0e90698c1449b0d02e6dccc2
upio <75510171+notpoiu@users.noreply.github.com>
2024-12-01 00:07:53 -0500

    fix: utc

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function replace_dynamic_variables(str: string, file_data: { pathname: string, size: number, uploadedAt: Date }, uploaded_files: number) {
-  return str.replaceAll("%file_name%", file_data.pathname).replaceAll("%file_size%", Math.floor((file_data.size / 1024 / 1024) * 100) / 100 + "MB").replaceAll("%uploaded_files%", uploaded_files.toString()).replaceAll("%file_upload_timestamp%", `${file_data.uploadedAt.toLocaleString()} UTC${file_data.uploadedAt.getTimezoneOffset()}`);
+  return str.replaceAll("%file_name%", file_data.pathname).replaceAll("%file_size%", Math.floor((file_data.size / 1024 / 1024) * 100) / 100 + "MB").replaceAll("%uploaded_files%", uploaded_files.toString()).replaceAll("%file_upload_timestamp%", `${file_data.uploadedAt.toLocaleString()} ${file_data.uploadedAt.getTimezoneOffset() == 0 ? "0 UTC" : "UTC" + file_data.uploadedAt.getTimezoneOffset()}`);
 }
